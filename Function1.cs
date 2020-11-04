26f0621a64f9d608f67904a7dc1179140426ff46
Andrew Manca <anmanca@microsoft.com>
2020-11-04 16:22:34 -0500

    Updating for session enabled functions

--- Function1.cs
+++ Function1.cs
@@ -9,8 +9,9 @@
 //    {
 
 //        //This will re-add the message to the queue using a Service Bus trigger on the deadletter queue
+//        //If you are using a Session Enabled Service Bus, please uncomment line of code for IsSessionEnabled
 //     [FunctionName("Function1")]
-//     [return: ServiceBus("%myqueuereal%", Connection = "serviceBusConnectionString")]
+//     [return: ServiceBus("%myqueuereal%", Connection = "serviceBusConnectionString /*, IsSessionEnabled = true */")]
 //        public static string Run([ServiceBusTrigger("%myqueue%", Connection = "serviceBusConnectionString")] string myQueueItem, ILogger log)
 //    {
 //        log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
--- Function3.cs
+++ Function3.cs
@@ -7,9 +7,10 @@ namespace FunctionApp6
 {
     public static class Function3
     {
-        //This is a test function to force messages to the DLQ
+        // This is a test function to force messages to the DLQ
+        // If you are using a Session Enabled Service Bus, please uncomment line of code for IsSessionEnabled
         [FunctionName("Function3")]
-        public static void Run([ServiceBusTrigger("%myqueuereal%", Connection = "ServiceBusConnectionString")] string myQueueItem, ILogger log)
+        public static void Run([ServiceBusTrigger("%myqueuereal%", Connection = "ServiceBusConnectionString"/*, IsSessionsEnabled =true*/)] string myQueueItem, ILogger log)
         {
 
             log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
