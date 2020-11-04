a585c59cd54da6eca3b6bf9c1bad9586d59bb31d
Andrew Manca <anmanca@microsoft.com>
2020-11-04 11:23:54 -0500

    creating test branch

--- Function4.cs
+++ Function4.cs
@@ -1,62 +1,62 @@
-using System;
-using System.IO;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.Azure.ServiceBus;
-using Microsoft.Azure.ServiceBus.Core;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
-using Microsoft.Extensions.Logging;
+//using System;
+//using System.IO;
+//using System.Text;
+//using System.Threading;
+//using System.Threading.Tasks;
+//using Microsoft.Azure.ServiceBus;
+//using Microsoft.Azure.ServiceBus.Core;
+//using Microsoft.Azure.WebJobs;
+//using Microsoft.Azure.WebJobs.Host;
+//using Microsoft.Extensions.Logging;
 
 
-namespace FunctionApp6
-{
+//namespace FunctionApp6
+//{
 
-    public static class Function4
-    {
+//    public static class Function4
+//    {
 
-        const string ServiceBusConnectionString = "ConnectionString";
-        const string QueueName = "QueueName";
-        const string QueueNameDeadletter = "QueueName/$deadletterqueue";
-        static IQueueClient queueClient;
-        static IQueueClient deadLetterQueueClient;
-        const int MaximumDeliveryCount = 11;
+//        const string ServiceBusConnectionString = "ConnectionString";
+//        const string QueueName = "QueueName";
+//        const string QueueNameDeadletter = "QueueName/$deadletterqueue";
+//        static IQueueClient queueClient;
+//        static IQueueClient deadLetterQueueClient;
+//        const int MaximumDeliveryCount = 11;
 
-        [FunctionName("Function4")]
-        public static async Task Run([TimerTrigger("0 */2 * * * *")] TimerInfo myTimer, ILogger log)
-        {
-            try 
-            { 
-            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
-            deadLetterQueueClient = new QueueClient(ServiceBusConnectionString, QueueNameDeadletter);
-            log.LogInformation($"message");
-            var receiver = new MessageReceiver(ServiceBusConnectionString, QueueNameDeadletter, ReceiveMode.ReceiveAndDelete);
+//        [FunctionName("Function4")]
+//        public static async Task Run([TimerTrigger("0 */2 * * * *")] TimerInfo myTimer, ILogger log)
+//        {
+//            try 
+//            { 
+//            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+//            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+//            deadLetterQueueClient = new QueueClient(ServiceBusConnectionString, QueueNameDeadletter);
+//            log.LogInformation($"message");
+//            var receiver = new MessageReceiver(ServiceBusConnectionString, QueueNameDeadletter, ReceiveMode.ReceiveAndDelete);
 
-            var messages = await receiver.ReceiveAsync(30);
-            for (int i = 0; i < messages.Count; i++)
-            {
-                log.LogInformation("sending message " + i);
-                //Only send messages that have a delivery count less than the count set
-                if (messages[i].SystemProperties.DeliveryCount < MaximumDeliveryCount)
-                {
-                    var message = Encoding.UTF8.GetString(messages[i].Body);
-                    var newMessage = new Message(Encoding.UTF8.GetBytes(message));
-                    await queueClient.SendAsync(newMessage);
-                }
+//            var messages = await receiver.ReceiveAsync(30);
+//            for (int i = 0; i < messages.Count; i++)
+//            {
+//                log.LogInformation("sending message " + i);
+//                //Only send messages that have a delivery count less than the count set
+//                if (messages[i].SystemProperties.DeliveryCount < MaximumDeliveryCount)
+//                {
+//                    var message = Encoding.UTF8.GetString(messages[i].Body);
+//                    var newMessage = new Message(Encoding.UTF8.GetBytes(message));
+//                    await queueClient.SendAsync(newMessage);
+//                }
 
-            }
-            await queueClient.CloseAsync();
-            await deadLetterQueueClient.CloseAsync();
-            log.LogInformation("Finished");
-            }
-            catch (Exception ex)
-            {
-                log.LogInformation($"Exception: {ex}");
-            }
+//            }
+//            await queueClient.CloseAsync();
+//            await deadLetterQueueClient.CloseAsync();
+//            log.LogInformation("Finished");
+//            }
+//            catch (Exception ex)
+//            {
+//                log.LogInformation($"Exception: {ex}");
+//            }
 
-        }
+//        }
 
-    }
-}
+//    }
+//}
