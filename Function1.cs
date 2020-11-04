e216d56ed796648244272e239f5f75c66d1b1d57
Andrew Manca <anmanca@microsoft.com>
2020-11-04 11:24:54 -0500

    commenting out for test

--- Function1.cs
+++ Function1.cs
@@ -1,21 +1,21 @@
-using System;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
-using Microsoft.Extensions.Logging;
+//using System;
+//using Microsoft.Azure.WebJobs;
+//using Microsoft.Azure.WebJobs.Host;
+//using Microsoft.Extensions.Logging;
 
-namespace FunctionApp6
-{
-    public static class Function1
-    {
+//namespace FunctionApp6
+//{
+//    public static class Function1
+//    {
 
-        //This will re-add the message to the queue using a Service Bus trigger on the deadletter queue
-     [FunctionName("Function1")]
-     [return: ServiceBus("%myqueuereal%", Connection = "serviceBusConnectionString")]
-        public static string Run([ServiceBusTrigger("%myqueue%", Connection = "serviceBusConnectionString")] string myQueueItem, ILogger log)
-    {
-        log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+//        //This will re-add the message to the queue using a Service Bus trigger on the deadletter queue
+//     [FunctionName("Function1")]
+//     [return: ServiceBus("%myqueuereal%", Connection = "serviceBusConnectionString")]
+//        public static string Run([ServiceBusTrigger("%myqueue%", Connection = "serviceBusConnectionString")] string myQueueItem, ILogger log)
+//    {
+//        log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
 
-        return myQueueItem;
-    }
-}
-}
+//        return myQueueItem;
+//    }
+//}
+//}
